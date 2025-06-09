ad2983f8c8293ee113c530a04eed741e02ebd1e7
evanap003300 <evanap003300@gmail.com>
2025-06-08 21:52:31 -0400

    Updated kerenl code

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -21,12 +21,11 @@ KERNEL_SEGMENT equ 0x1000
     ; Switches to 32 bit protected mode
     call switch_to_pm
 
-
 read_disk:
     mov bx, KERNEL_SEGMENT          
 
     mov ah, 2       ; Read sectors
-    mov al, 1       ; Number of sectors to read can store this in dh and make it a new asm file 
+    mov al, 2       ; Number of sectors to read 
     mov ch, 0       ; Cylinder number
     mov dh, 0       ; Head number
     mov cl, 2       ; Sector number  
@@ -38,15 +37,14 @@ read_disk:
     ret
 
 disk_error:
-    cli ; Disable interupts 
+    cli             ; Disable interupts 
 .halt:
-    hlt        ; halt cpu
-    jmp .halt  ; prevent further execution by running hlt infinitly 
+    hlt             ; halt cpu
+    jmp .halt       ; prevent further execution by running hlt infinitly 
 
 %include "bootloader/gdt.asm"
 %include "bootloader/switch_to_pm.asm"
 %include "bootloader/print_string_pm.asm"
-%include "bootloader/print_string.asm"
 
 [bits 32]
 
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,66 @@
+#include "stdint.h"
+
+#define VGA_MEMORY 0xB8000
+#define BLACK 0
+#define GREEN 2
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25 
+
+// add this to header file
+void print_char(char c);
+void clear_screen();
+void hello_world();
+void print_char_at_pos(char c, int x, int y);
+
 void main() {
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 'A';
+    // hello_world(6);
+
+    // implement a cursor tonight 
+
+    print_char_at_pos('x', 40, 12); // print an x in the middle of the screen 
+
+    // Your bss segment is probably set up incorrectly becuase the gloabal variables don't work as intended. 
+
+    // print_char('A');
+    // clear_screen();
+}
+
+void hello_world(int len) {
+    volatile uint16_t* vga = (uint16_t*) VGA_MEMORY;
+    const uint16_t color = (GREEN << 8) | (BLACK << 12); // first 4 bytes: background, then foreground, then character
+    
+    char s[] = "Hello"; // This works it just has to be less than like 10 bytes 
+    vga--;
+
+    for(int i = 0; i < len; i++) {
+        *vga = s[i] | color;
+        vga++;
+    }
+}
+
+void print_char(char c) {
+    volatile uint16_t* vga = (uint16_t*) VGA_MEMORY;
+    const uint16_t color = (GREEN << 8) | (BLACK << 12); // first 4 bytes: background, then foreground, then character
+    *vga = c | color;
+}
+
+void print_char_at_pos(char c, int x, int y) {
+    volatile uint16_t* vga = (uint16_t*) VGA_MEMORY;
+    const uint16_t color = (GREEN << 8) | (BLACK << 12); // first 4 bytes: background, then foreground, then character
+    for (int i = 0; i < y * SCREEN_WIDTH + x; i++) {
+        vga++;
+    }
+    *vga = c | color; 
+}
+
+
+// Works 
+void clear_screen() {
+    uint16_t* vga = (uint16_t*) VGA_MEMORY;
+    const uint16_t color = (GREEN << 8) | (BLACK << 12); // first 4 bytes: background, then foreground, then character
+    
+    for (uint16_t i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
+            *vga = ' ' | color;
+            vga++;
+    }
 }
