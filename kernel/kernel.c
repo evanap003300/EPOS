840d04e6dc9b835f9b32bcd83b1664fb37735869
evanap003300 <evanap003300@gmail.com>
2025-06-10 19:22:50 -0400

    Updated kernel code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -18,18 +18,19 @@ void _start() {
 }
 
 void main() {
-    // testing git 
     // you can return values, but global variables and including other functions from other files doesn't work yet.
     // the warp agent was helpful in fixing my previous errors
 
-    // Have some manner of "CRT" (C run-time) code executes .data initialization to fix global variables
+    // if you want to get other files to work you need to change how you compile 
 
-    // Prompt for agent: I'm working on my operating system and have found that my global variables don't work in my code, I'm unable to pass strings or arrays in as parameters to my functions, and I'm unable to use functions declared in other files than kernel. How can I fix this given my code?
+    // Have some manner of "CRT" (C run-time) code executes .data initialization to fix global variables
 
     const uint16_t color = (GREEN << 8) | (BLACK << 12); // first 4 bytes: background, then foreground, then character
     volatile uint16_t* vga = (uint16_t*) VGA_MEMORY;
-    hello_world(5, color, vga);
+    // hello_world(5, color, vga);
+    // test_asm();
 
+    print_int(12345, 5, color, vga);
     // clear_screen(color, vga);
 
     // print_char_at_pos('x', 40, 12); // print an x in the middle of the screen 
@@ -49,7 +50,7 @@ void hello_world(int len, uint16_t color, uint16_t* vga) {
 
 void print_char(char c) {
     volatile uint16_t* vga = (uint16_t*) VGA_MEMORY;
-    const uint16_t color = (GREEN << 8) | (BLACK << 12); // first 4 bytes: background, then foreground, then character
+    const uint16_t color = (GREEN << 8) | (BLACK << 12); // first 4 bytes: backgroun.ld -o bin/kernel.bin build/kernel.o build/myasm.o --oformat bina, then foreground, then character
     *vga = c | color;
 }
 
@@ -66,4 +67,42 @@ void clear_screen(uint16_t color, uint16_t* vga) {
     for (uint16_t i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
             vga[i] = ' ' | color;
     }
+}
+
+void test_asm() {
+    uint32_t a = 7;
+    uint32_t b = 2;
+    uint32_t result;
+
+    asm volatile (
+        "addl %%ebx, %%eax;"
+        : "=a" (result)
+        : "a" (a), "b" (b)
+    );
+
+    if (result < 10)
+        print_char('0' + result);  // Should print '8'
+    else
+        print_char('?');  // Error case
+}
+
+// works up to 5 digits
+void print_int(uint16_t num, uint16_t digits, uint16_t color, uint16_t* vga) {
+    int digit = 0;
+    int ten_pow = 1;
+    int copy = num;
+
+    for(int i = 0; i < digits - 1; i++) {
+        ten_pow *= 10;
+    }
+
+    int i = 0;
+  
+    while(copy > 0) {
+         digit = (num / ten_pow) % 10;
+         vga[i] = ('0' + digit) | color;
+         i++;
+         ten_pow /= 10;
+         copy /= 10;
+     }
 }
--- kernel/screen.h
+++ kernel/screen.h
@@ -5,5 +5,7 @@ void print_char(char c);
 void clear_screen(uint16_t color, uint16_t* vga);
 void hello_world(int len, uint16_t color, uint16_t* vga);
 void print_char_at_pos(char c, int x, int y);
+void test_asm();
+void print_int(uint16_t num, uint16_t digits, uint16_t color, uint16_t* vga);
 
 #endif
