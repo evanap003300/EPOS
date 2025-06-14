98934ca2196faec17843a34b8ea5ec411db0e14c
Evan Phillips <146125960+evanap003300@users.noreply.github.com>
2025-06-13 20:35:45 -0400

    Update screen.h

--- kernel/screen.h
+++ kernel/screen.h
@@ -8,5 +8,11 @@ void print_char_at_pos(char c, int x, int y);
 void test_asm();
 void print_int(uint16_t num, uint16_t digits, uint16_t color, uint16_t* vga);
 uint16_t number_of_digits(uint16_t n);
+void print_E(uint16_t color, uint16_t* vga);
+void print_P(uint16_t color, uint16_t* vga);
+void print_O(uint16_t color, uint16_t* vga);
+void print_S(uint16_t color, uint16_t* vga);
+void print_intro(uint16_t color, uint16_t* vga);
+void print_logo(uint16_t color, uint16_t* vga);
 
 #endif
